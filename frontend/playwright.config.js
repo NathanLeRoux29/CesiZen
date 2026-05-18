1578534c31e0eccd9bbc54cf734c60adbef5eb4f
NathanleRoux29 <nathanlerouxpro@gmail.com>
2026-05-18 15:08:11 +0200

    ajout des tests Playwright dans la CI

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -44,9 +44,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run dev',
-  //   port: 3000,
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run dev',
+    port: 3000,
+    reuseExistingServer: !process.env.CI,
+    timeout: 60 * 1000,
+  },
 });
